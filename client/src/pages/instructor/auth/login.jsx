1a445c0a6c779570602528ce7ac3d91063454742
Jodking <tripathihitesh580@gmail.com>
2025-04-21 17:54:27 +0530

    name update

--- client/src/pages/instructor/auth/login.jsx
+++ client/src/pages/instructor/auth/login.jsx
@@ -83,7 +83,7 @@ const InstructorLoginPage = () => {
       <header className="px-4 lg:px-6 h-14 flex items-center justify-between border-b">
         <Link to={"/"} className="flex items-center justify-center">
           <GraduationCap className="h-8 w-8 mr-4" />
-          <span className="font-extrabold text-xl">LMS work</span>
+          <span className="font-extrabold text-xl">LMS LEARN</span>
         </Link>
         <div className="flex items-center justify-center gap-4">
           <Link
